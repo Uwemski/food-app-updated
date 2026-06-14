fc099b738a98c20ae2e477a2d685d642d471152c
Uwemski <iconuwemfrank@gmail.com>
2026-06-14 21:12:10 +0100

    class names can't have hyphens

--- app/View/Components/Nav/admin-nav.php
+++ app/View/Components/Nav/admin-nav.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Admin_nav extends Component
+class AdminNav extends Component
 {
     /**
      * Create a new component instance.
