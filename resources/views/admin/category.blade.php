f36469259cbe0f3a51262907df2cdd167b763ab8
Uwemski <iconuwemfrank@gmail.com>
2026-09-08 18:29:33 +0100

    delete async await method done for Categor, don't see any need for update method

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -22,7 +22,8 @@
         </thead>
         <tbody class="divide-y divide-soft/10">
             @foreach($categories as $category)
-            <tr class="hover:bg-cream/40 transition-colors duration-200">
+            <!-- id should be here to target this row below -->
+            <tr class="hover:bg-cream/40 transition-colors duration-200" id="category-{{$category->id}}">
                 <td class="px-6 py-5 text-muted"> {{$loop->iteration}}</td>
                 <td class="px-6 py-5 font-semibold text-charcoal">{{$category->name}}</td>
                 <td class="px-6 py-5 text-muted">{{$category->created_at->format('Y-m-d')}}</td>
@@ -30,7 +31,13 @@
                     <form action="{{route('category.destroy', $category->id)}}" method='POST'>
                         @csrf
                         @method('delete')
-                        <button class='text-red-500'>Delete</button>
+                        <button class='text-red-500' 
+                            onclick="deleteResource(
+                                {{ $category->id }},
+                                '{{ route('category.destroy', $category->id) }}'
+                            )">
+                            Delete
+                        </button>
                     </form>
                 </td>
                 
@@ -38,5 +45,31 @@
             @endforeach
         </tbody>
     </table>
-     
+     <script>
+        async function deleteResource(categoryId){
+            try{
+                // hopefully this doe it
+                const response = await fetch(`{{route('category.destroy', $category->id')}}`, {
+                    method: 'DELETE',
+                    header: {
+                        'Content-type' : 'application/json',
+                        'X-CSRF-TOKEN': '{{csrf_token()}}'
+                    }
+                })
+
+                if(!response.ok){
+                    throw new Error('Failed to delete the resource');
+                }
+
+                const data = await response.json();
+                console.log(data.message)
+                const row = document.getElementById(`category-${$categoryId}`).remove();
+                if(row){
+                    row.remove
+                }
+            }catch(error){
+                console.error('Error:', error)
+            }
+        }
+     </script>
 </x-admin-layout>
